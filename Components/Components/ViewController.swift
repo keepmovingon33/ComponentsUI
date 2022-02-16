fc4d8aa9a8dea02a0e435c1370e65f3917adfc6e
sky <anvanc37@gmail.com>
2022-02-16 06:26:26 -0500

    setup

---
+++ Components/Components/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Components
+//
+//  Created by sky on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
